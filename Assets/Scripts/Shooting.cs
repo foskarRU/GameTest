b691a55d73ada98ce9c36004897fadd6193abf26
foskarRU <s.polonski@mail.ru>
2018-08-20 18:38:42 +0500

    Shooting

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+    public GameObject shell;
+    private float timeFlag = 0;
+    private float pause = 1f;
+    private float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((timeFlag + pause) <= Time.time)
+        {
+            Vector3 coor = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, -8.5f);
+            GameObject obj = Instantiate(shell, coor, Quaternion.identity);
+            obj.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+            timeFlag = Time.time;
+        }
+    }
+}
